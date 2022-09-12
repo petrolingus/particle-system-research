f9d9d58019702375ee77dc8716507e5b39acaa08
Aleksandr A. Lebedev <aleksander.a.lebedev@netcracker.com>
2022-09-12 04:22:43 +0300

    fix

--- src/main/java/me/petrolingus/unn/test/Controller.java
+++ src/main/java/me/petrolingus/unn/test/Controller.java
@@ -8,8 +8,7 @@ import java.util.List;
 
 public class Controller {
 
-    public StackedAreaChart<Number, Number> positionChart;
-    public StackedAreaChart<Number, Number> chart2;
+    public StackedAreaChart<Number, Number> interactionEnergyChart;
 
     public TextField epsilonField;
     public TextField sigmaField;
@@ -18,61 +17,48 @@ public class Controller {
     public TextField hopField;
 
     public void initialize() {
-        Algorithm algorithm = new Algorithm();
-        List<List<Particle>> snapshots = algorithm.run();
 
-        if (!positionChart.getData().isEmpty()) {
-            positionChart.getData().clear();
-        }
-
-        for (int i = 0; i < snapshots.size(); i++) {
-            XYChart.Series<Number, Number> positions = new XYChart.Series<>();
-            for (int j = 0; j < snapshots.get(0).size(); j++) {
-                double x = snapshots.get(i).get(j).x;
-                double y = snapshots.get(i).get(j).y;
-                positions.getData().add(new XYChart.Data<>(x, y));
-            }
-            positionChart.getData().add(positions);
-        }
-
+        onButton();
     }
 
     public void onButton() {
+        drawInteractionEnergyChart();
+    }
 
+    private void drawInteractionEnergyChart() {
         double a = Double.parseDouble(aField.getText());
         double b = Double.parseDouble(bField.getText());
         double h = Double.parseDouble(hopField.getText());
         int n = (int) Math.floor((b - a) / h);
 
         XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.setName("U");
         for (int i = 0; i < n; i++) {
             double x = a + i * h;
             double y = U(x);
             series.getData().add(new XYChart.Data<>(x, y));
         }
-        if (!chart2.getData().isEmpty()) {
-            chart2.getData().clear();
+        if (!interactionEnergyChart.getData().isEmpty()) {
+            interactionEnergyChart.getData().clear();
         }
-        chart2.getData().add(series);
-
-        XYChart.Series<Number, Number> series2 = new XYChart.Series<>();
-        for (int i = 0; i < n; i++) {
-            double x = a + i * h;
-            double y = -F(x);
-            series2.getData().add(new XYChart.Data<>(x, y));
-        }
-//        if (!chart2.getData().isEmpty()) {
-//            chart2.getData().clear();
-//        }
-        chart2.getData().add(series2);
-
+        interactionEnergyChart.getData().add(series);
     }
 
     private double U(double r) {
         double epsilon = Double.parseDouble(epsilonField.getText());
         double sigma = Double.parseDouble(sigmaField.getText());
+        double r0 = sigma;
+        double r1 = 1.1 * r0;
+        double r2 = 1.8 * r0;
+        double K = 0;
+        if (r < r1) {
+            K = 1;
+        } else if (r < r2) {
+            K = Math.pow(1.0 - Math.pow((r - r1)/(r1 - r2), 2), 2);
+        }
+        sigma = r0 / Math.pow(2, 1.0/6.0);
         double sr = Math.pow(sigma / r, 6);
-        return 4.0 * epsilon * (sr * sr - sr);
+        return 4.0 * epsilon * (sr * sr - sr) * K;
     }
 
     private double F(double r) {
